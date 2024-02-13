e451eb38f1f77a2ed3837af9f654310a4673ff1f
yeoeol <gnsl3528@naver.com>
2024-02-13 19:25:12 +0900

    programmers_푸드 파이트 대회, 햄버거 만들기

---
+++ programmers/food_fight_contest.py
@@ -0,0 +1,9 @@
+def solution(food):
+    answer = ''
+
+    for i in range(1, len(food)):
+        n = food[i] // 2
+        answer += str(i)*n
+    answer = '0' + answer[::-1]
+
+    return answer
---
+++ programmers/making_hamburger.py
@@ -0,0 +1,15 @@
+def solution(ingredient):
+    answer = 0
+    stack = []
+    for i in ingredient:
+        stack.append(i)
+
+        if stack[-4:] == [1,2,3,1]:
+            answer += 1
+            del stack[-4:]
+    return answer
+
+
+ingredient = [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1]
+print(solution(ingredient))
+
